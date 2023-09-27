22049880d628dc0a346cb198d44ce05a641b2f8f
Paulo César Moraes <moraeswd@gmail.com>
2023-09-27 19:38:42 -0300

    feat: adding revalidator

--- src/app/exames/route.js
+++ src/app/exames/route.js
@@ -1,1 +1,9 @@
+export const dynamic = 'auto'
+export const dynamicParams = true
 export const revalidate = 60
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
+export const maxDuration = 5
+ 
+export default function MyComponent() {}
