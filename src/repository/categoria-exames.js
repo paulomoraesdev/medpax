20bd440b2b79e13e81eab5d371652737b3c44b6c
Paulo Moraes <paulo.moraes@coachy.net>
2024-06-18 19:08:59 -0300

    updating categories

--- src/repository/categoria-exames.js
+++ src/repository/categoria-exames.js
@@ -3,4 +3,48 @@ export default [
     key: "endoscopia",
     title: "Endoscopia",
   },
+  {
+    key: "laboratoriais",
+    title: "Laboratoriais",
+  },
+  {
+    key: "raio-x",
+    title: "Raio X",
+  },
+  {
+    key: "ressonancia-magnetica",
+    title: "Ressonância Magnética",
+  },
+  {
+    key: "colonoscopia",
+    title: "Colonoscopia",
+  },
+  {
+    key: "ultrassom",
+    title: "Ultrassom",
+  },
+  {
+    key: "panoramica",
+    title: "Panorâmica",
+  },
+  {
+    key: "ecocardiograma",
+    title: "Ecocardiograma",
+  },
+  {
+    key: "eletroencefalograma",
+    title: "Eletroencefalograma",
+  },
+  {
+    key: "holter",
+    title: "Holter",
+  },
+  {
+    key: "doppler",
+    title: "Doppler",
+  },
+  {
+    key: "eletrocardiograma",
+    title: "Eletrocardiograma",
+  },
 ];
