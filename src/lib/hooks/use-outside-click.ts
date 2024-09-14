a21c810f156959c14ba28a4fb34949a41d1f0b35
Paulo Moraes <paulo.moraes@coachy.net>
2024-09-14 19:45:18 -0300

    feat: updating

--- src/lib/hooks/use-outside-click.ts
+++ src/lib/hooks/use-outside-click.ts
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
  
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  callback: any
 ) => {
   useEffect(() => {
     const listener = (event: any) => {
