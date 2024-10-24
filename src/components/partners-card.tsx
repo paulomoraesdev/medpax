1ab459c104c8085efb26952e8d23b830d5c1bc5a
Paulo Moraes <accounts@paulomoraes.dev>
2024-10-24 20:30:52 -0300

    updarting

--- src/components/partners-card.tsx
+++ src/components/partners-card.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function PartnersCard({ partner, identifier }: { partner: any, identifier: any }) {
   return (
-    <Link href={ `/parceiros/${ identifier }` } className="whitespace-normal break-words rounded-lg border border-blue-gray-50 bg-white p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none">
+    <Link href={ `/parceiros/${ identifier }` } className="whitespace-normal break-words hover:scale-105 transition rounded-lg border border-blue-gray-50 bg-white p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none">
 				<div className="flex justify-start item-center gap-1">
           { partner.categoria?.map((categoria: any, index: any) => (<div className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block" key={ index }>{categoria.fields?.nome}</div>)) }
 				</div>
@@ -22,12 +22,21 @@ export default function PartnersCard({ partner, identifier }: { partner: any, id
             </p>
           )
         }
-        <footer className="flex items-center justify-between mt-4">
+        <footer className="flex items-center justify-end gap-2 mt-4">
           {
             partner.website && (
               <button onClick={ (ev) => { ev.preventDefault(); ev.stopPropagation(); window.open(partner.website); } }>
-                <p className="px-4 py-2 bg-teal-700 hover:bg-teal-900 text-white text-sm font-medium rounded-md transition-all duration-300">
-                  Acessar o site
+                <p className="px-4 py-2 bg-teal-700 border border-teal-700 hover:bg-teal-900 text-white text-sm font-medium rounded-md transition-all duration-300">
+                  Site
+                </p>
+              </button>
+            )
+          }
+          {
+            partner.midiasocial && (
+              <button onClick={ (ev) => { ev.preventDefault(); ev.stopPropagation(); window.open(partner.midiasocial); } }>
+                <p className="px-4 py-2 text-teal-700 border border-teal-700 bg-transparent hover:bg-lime-500 hover:text-white hover:border-lime-500 text-sm font-medium rounded-md transition-all duration-300">
+                  Mídia Social
                 </p>
               </button>
             )
--- src/components/professional-card.tsx
+++ src/components/professional-card.tsx
@@ -18,10 +18,8 @@ export default function ProfessionalCard({ professional, identifier }: { profess
     }
   }
 
-  const Component = professional.assinante ? Link : 'article'
-
   return (
-    <Component href={ `/profissionais/${ identifier }` } className="whitespace-normal break-words rounded-lg border border-blue-gray-50 bg-white p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none">
+    <article className="whitespace-normal break-words rounded-lg border border-blue-gray-50 bg-white p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none">
 				<div className="flex justify-start item-center">
           { professional.specialties?.map((specialty: any, index: any) => (<div className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block" key={ index }>{specialty.fields?.title}</div>)) }
 				</div>
@@ -57,7 +55,16 @@ export default function ProfessionalCard({ professional, identifier }: { profess
               </button>
             )
           }
+          {
+            professional.assinante && (
+              <Link href={ `/profissionais/${ identifier }` }>
+                <p className="px-4 py-2 bg-teal-700 hover:bg-teal-900 text-white text-sm font-medium rounded-md transition-all duration-300">
+                  Mais Detalhes
+                </p>
+              </Link>
+            )
+          }
         </footer>
-    </Component>
+    </article>
   )
 }
